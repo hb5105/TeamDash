ffe7b5878ac83723a71dcedc21cfb9c48f5d09c3
vasvi1203 <vasvigup@usc.edu>
2023-10-14 19:48:50 -0700

    Working on word generation per player

--- Assets/Scripts/WordGenerator.cs
+++ Assets/Scripts/WordGenerator.cs
@@ -9,37 +9,21 @@ public class WordGenerator : MonoBehaviour
     public TextMeshProUGUI textBox; // Reference to the Text element in the Inspector
 
     // List of random words to choose from
-    private string[] randomWords = { "CAT", "CAR", "HAT", "HIT", "MAT", "DOG", "SUN", "PEN", "BUS", "SKY" };
-
-    // Static variable to store the initial word
-    private static string initialWord = null;
+    private string[] randomWords = { "SPACE", "PLANET", "EARTH", "MARS" };
 
     private void Start()
     {
-        if (textBox == null)
+        if (textBox != null)
+        {
+            // Get a random word from the array
+            string randomWord = randomWords[Random.Range(0, randomWords.Length)];
+
+            // Set the Text element's text to the random word
+            textBox.text = randomWord;
+        }
+        else
         {
             Debug.LogError("Text element not assigned in the Inspector.");
-            return;
         }
-
-        // If initialWord is null, assign a random word to it
-        if (initialWord == null)
-        {
-            initialWord = GetRandomWord();
-        }
-
-        textBox.text = initialWord;
-    }
-
-    // Method to get a random word
-    private string GetRandomWord()
-    {
-        return randomWords[Random.Range(0, randomWords.Length)];
-    }
-
-    // Call this method when the player successfully guesses the word
-    public void OnWordCompleted()
-    {
-        textBox.text = GetRandomWord();
     }
 }
