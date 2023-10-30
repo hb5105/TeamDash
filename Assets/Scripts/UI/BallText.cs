d26c49949d342ad63377cd6041b473e51526c765
vasvi1203 <vasvigup@usc.edu>
2023-10-30 15:23:28 -0700

    Fixed ball spawning error

--- Assets/Scripts/UI/BallText.cs
+++ Assets/Scripts/UI/BallText.cs
@@ -15,25 +15,32 @@ public class BallText : MonoBehaviour
     //     int idx = Random.Range(0, gameManager.word.Length);
     //     tmp.SetText(gameManager.word[idx].ToString().ToUpper());
     // }
+    
     public void Start()
     {
-        if (ballTextObj == null) 
+        if (ballTextObj == null)
         {
             Debug.LogError("ballTextObj is null!");
             return;
         }
-    
+
         tmp = ballTextObj.GetComponent<TextMeshPro>();
-    
-        if (tmp == null) 
+
+        if (tmp == null)
         {
             Debug.LogError("tmp is null (TextMeshPro component not found)!");
             return;
         }
 
-        if (gameManager == null) 
+        if (gameManager == null)
         {
             Debug.LogError("gameManager is null!");
+            return;
+        }
+
+        if (gameManager.remainingChars == null || gameManager.remainingChars.Count == 0)
+        {
+            Debug.LogError("remainingChars is null or empty!");
             return;
         }
 
