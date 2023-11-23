de347effeef7358c92547f96ced9a95079933160
Hritik Ajay Bansal <43716075+hb5105@users.noreply.github.com>
2023-11-22 17:24:32 -0800

    MoveOpponent Bug Fix (#103)

--- Assets/Scripts/PowerUps/PaddleMovePowerUp.cs
+++ Assets/Scripts/PowerUps/PaddleMovePowerUp.cs
@@ -2,15 +2,15 @@ using UnityEngine;
 
 public class PaddleMovePowerUp : MonoBehaviour
 {
-    private Paddle paddle;
+    private Paddle paddle;  
     private Paddle opponentPaddle;
 
+    public GameObject northWall; // Variable for the north wall
+    public GameObject southWall; // Variable for the south wall
+
     public float shiftDistance = 3.0f;
-    public float powerUpDuration = 5f;
     public float maxYPosition = 4.5f;
     public float minYPosition = -4.5f;
-
-    private Vector2 originalOpponentPosition;
 
     private static bool powerUpActiveGlobal = false;
 
@@ -24,21 +24,35 @@ public class PaddleMovePowerUp : MonoBehaviour
 
         FindOpponentPaddle();
     }
-
-    public void ShiftOpponentPosition()
+public void ShiftOpponentPosition()
+{
+    if ( opponentPaddle)
     {
-        if (!powerUpActiveGlobal && opponentPaddle)
+        powerUpActiveGlobal = true;
+
+        // Get the Y-coordinate of the opponent's paddle
+        float opponentY = opponentPaddle.transform.position.y;
+
+        // Determine if the opponent is above or below the center (0,0)
+        bool isAboveCenter = opponentY > 0;
+
+        // Calculate new Y position based on the opponent's position relative to the center
+        float newYPosition;
+        if (isAboveCenter)
         {
-            powerUpActiveGlobal = true;
-
-            originalOpponentPosition = opponentPaddle.transform.position;
-            float newYPosition = originalOpponentPosition.y + shiftDistance;
-            newYPosition = Mathf.Clamp(newYPosition, minYPosition, maxYPosition);
-            opponentPaddle.transform.position = new Vector2(originalOpponentPosition.x, newYPosition);
-
-            Invoke("ResetOpponentPosition", powerUpDuration);
+            // If the opponent is above the center, move towards the south wall
+            newYPosition = Mathf.Max(opponentY - shiftDistance, minYPosition);
         }
+        else
+        {
+            // If the opponent is below the center, move towards the north wall
+            newYPosition = Mathf.Min(opponentY + shiftDistance, maxYPosition);
+        }
+
+        // Move the paddle
+        opponentPaddle.transform.position = new Vector2(opponentPaddle.transform.position.x, newYPosition);
     }
+}
 
     private void FindOpponentPaddle()
     {
@@ -53,12 +67,5 @@ public class PaddleMovePowerUp : MonoBehaviour
         }
     }
 
-    private void ResetOpponentPosition()
-    {
-        if (opponentPaddle)
-        {
-            opponentPaddle.transform.position = originalOpponentPosition;
-            powerUpActiveGlobal = false;
-        }
-    }
+    // The ResetOpponentPosition method is no longer needed and has been removed
 }
